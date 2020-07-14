73981b5d2df2adf8ce7482fa1614db385dea8541
FaceWallHan <1428528183@qq.com>
2020-07-14 22:46:07 +0800

    虽然代码完成了，但是出了错误，应该是与内容提供者的连接字符串有误造成的

--- app/src/main/java/com/start/head/activity/ProviderActivity.java
+++ app/src/main/java/com/start/head/activity/ProviderActivity.java
@@ -1,9 +1,11 @@
 package com.start.head.activity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -12,15 +14,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.start.head.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProviderActivity extends AppCompatActivity {
     private Button add_data,query_data,update_data,delete_data;
     private ListView provider_list;
-    private String newId,uriString="content://example.databasetest.provider/book";
+    private String newId,uriString="content://com.example.databasetest.provider/book";
+    private List<String>list;
+    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.provider_layout);
         inView();
+        setListener();
     }
     private void setListener(){
         add_data.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +42,25 @@ public class ProviderActivity extends AppCompatActivity {
                 values.put("price",22.85);
                 Uri newUri=getContentResolver().insert(uri,values);
                 newId=newUri.getPathSegments().get(1);
+            }
+        });
+        query_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri=Uri.parse(uriString);
+                Cursor cursor=getContentResolver().query(uri,null,null,null,null);
+                if (cursor!=null){
+                    list.clear();
+                    while (cursor.moveToNext()){
+                        String name=cursor.getColumnName(cursor.getColumnIndex("name"));
+                        String author=cursor.getColumnName(cursor.getColumnIndex("author"));
+                        int pages=cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price=cursor.getDouble(cursor.getColumnIndex("price"));
+                        list.add("名称："+name+"作者："+author+"页数："+pages+"价格"+price);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+                //cursor.close();
             }
         });
         update_data.setOnClickListener(new View.OnClickListener() {
@@ -61,5 +88,8 @@ public class ProviderActivity extends AppCompatActivity {
         update_data=findViewById(R.id.update_data);
         delete_data=findViewById(R.id.delete_data);
         provider_list=findViewById(R.id.provider_list);
+        list=new ArrayList<>();
+        adapter=new ArrayAdapter<String>(ProviderActivity.this,android.R.layout.simple_list_item_1,list);
+        provider_list.setAdapter(adapter);
     }
 }
