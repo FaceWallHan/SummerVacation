d2d849201c0a007162566997eb49b6862ac7e2f8
FaceWallHan <1428528183@qq.com>
2020-07-16 22:20:11 +0800

    通知的意图和进阶技巧

--- app/src/main/java/com/start/head/activity/NotificationActivity.java
+++ app/src/main/java/com/start/head/activity/NotificationActivity.java
@@ -2,8 +2,12 @@ package com.start.head.activity;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +17,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import com.start.head.R;
+
+import java.io.File;
 
 public class NotificationActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -28,6 +34,9 @@ public class NotificationActivity extends AppCompatActivity implements View.OnCl
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.send_notice:
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                PendingIntent pi=PendingIntent.getActivity(this,0,intent,0);
                 NotificationManager manager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 Notification notification=new NotificationCompat.Builder(this)
                         .setContentTitle("This is content title")
@@ -35,6 +44,14 @@ public class NotificationActivity extends AppCompatActivity implements View.OnCl
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.mipmap.ic_launcher)//显示在系统状态栏上的小图标
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))//下拉系统状态栏时的大图标
+                        .setContentIntent(pi)
+                        .setAutoCancel(true)
+                        .setSound(Uri.fromFile(new File("/system/media/audio/ringtones/Luna.ogg")))
+                        .setVibrate(new long[]{0,1000,1000,1000})
+                            //下标为0的值表示手机静止的时长，下标为1的值手机振动的时长，下标为2的值又表示手机静止的时长，以此类推
+                        .setLights(Color.GREEN,1000,1000)
+                                               //亮起的时长，暗去的时长
+                        .setDefaults(NotificationCompat.DEFAULT_ALL)//默认效果，根据当前手机环境来觉得
                         .build();
                 manager.notify(1,notification);
                 break;
