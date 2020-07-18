27ec854bd0cd81e2b8690e09a27672123a5e882f
FaceWallHan <1428528183@qq.com>
2020-07-18 22:12:07 +0800

    完成了打开摄像头拍照并显示出来的功能，想起了阳光校园

--- app/src/main/java/com/start/head/activity/CameraAlbumActivity.java
+++ app/src/main/java/com/start/head/activity/CameraAlbumActivity.java
@@ -1,22 +1,33 @@
 package com.start.head.activity;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.start.head.R;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class CameraAlbumActivity extends AppCompatActivity {
     private static final int TAKE_PHOTO=1;
+    private static final int CHOOSE_PHOTO=1;
     private ImageView picture;
     private Uri imageUri;
-    private Button take_photo;
+    private Button take_photo,choose_from_album;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +37,43 @@ public class CameraAlbumActivity extends AppCompatActivity {
         setListener();
     }
     private void setListener(){
-        take_photo.setOnClickListener(new View.OnClickListener() {
+        choose_from_album.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+            }
+        });
+        take_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File outputImage=new File(getExternalCacheDir(),"output_image.jpg");
+                //getExternalCacheDir--->手机SD卡的应用关联缓存目录下--->SD卡中专门用于存放当前应用缓存数据的位置
+                try {
+                    if (outputImage.exists()){
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                    //Result of 'File.delete()' is ignored???
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT>=24){
+                    imageUri= FileProvider.getUriForFile(CameraAlbumActivity.this,"com.start.head.activity",outputImage);
+                }else {
+                    //系统版本低于7.0
+                    imageUri=Uri.fromFile(outputImage);
+                }
+                //启动相机程序
+                Intent intent=new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(intent,TAKE_PHOTO);
             }
         });
     }
     private void inView(){
         picture=findViewById(R.id.picture);
         take_photo=findViewById(R.id.take_photo);
+        choose_from_album=findViewById(R.id.choose_from_album);
     }
 
     @Override
@@ -44,11 +82,22 @@ public class CameraAlbumActivity extends AppCompatActivity {
         switch (requestCode) {
             case TAKE_PHOTO:
                 if (resultCode==RESULT_OK){
-
+                    try {
+                        //将拍摄的图片显示出来
+                        Bitmap bitmap= BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
             default:
                 break;
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
