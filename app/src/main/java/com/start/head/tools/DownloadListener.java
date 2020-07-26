9b176f6a033879dc343ca257e1942acc2403d0eb
FaceWallHan <1428528183@qq.com>
2020-07-26 22:29:07 +0800

    下载示例的AsyncTask部分，RandomAccessFile？rw文件？

---
+++ app/src/main/java/com/start/head/tools/DownloadListener.java
@@ -0,0 +1,9 @@
+package com.start.head.tools;
+
+public interface DownloadListener {
+    void onProgress(int progress);
+    void onSuccess();
+    void onFailed();
+    void onPaused();
+    void onCanceled();
+}
